4215a5d965eca6a44f8501513d17074a90e55228
christianzaboli <zabolichristian@gmail.com>
2025-09-30 18:56:33 +0200

    creazione componente degli articoli, piccoli elementi in page per il form che verrá usato

---
+++ src/assets/articleList.jsx
@@ -0,0 +1,26 @@
+
+
+export default function articleList() {
+    const lista = [
+        {
+            id: 1,
+            title: 'Super Mario Galaxy + Super Mario Galaxy 2: doppio ritorno tra le stelle'
+        },
+        {
+            id: 2,
+            title: 'DanDaDan - Stagioni 1 e 2 - La recensione'
+        },
+        {
+            id: 3,
+            title: 'EA Sports FC 26 - La recensione'
+        },
+        {
+            id: 4,
+            title: 'Tutte le novità di Logitech dal G Play 2025'
+        },
+        {
+            id: 5,
+            title: 'Hollow Knight: Silksong - La recensione'
+        },
+    ]
+}
